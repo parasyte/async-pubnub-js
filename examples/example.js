720fd1f85f1f8cbd199e3212e7304a096f9e2c1f
Jay Oster <jay@kodewerx.org>
2023-03-18 15:06:13 -0700

    Simplify await timer

--- examples/example.js
+++ examples/example.js
@@ -18,22 +18,21 @@ async function subscribe_loop(stream) {
 /** @arg {Stream} stream - Produce messages into this stream */
 async function publish_loop(stream) {
   for (const msg of ['hello', 'world', { num: 123 }, 'done']) {
-    await timer(1000, async () => {
-      console.log(`Sending: ${inspect(msg)}`);
+    await timer(1000);
 
-      await stream.publish(msg);
-    });
+    console.log(`Sending: ${inspect(msg)}`);
+
+    await stream.publish(msg);
   }
 }
 
 /**
  * @arg {Number} delay - Delay in milliseconds
- * @arg {(value: any) => any} fn - Callback
  */
-export async function timer(delay, fn) {
+export async function timer(delay) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
-  }).then(fn);
+  });
 }
 
 const pubnub = new PubNub('demo', 'demo', await userId());
